b48fbf22f603f46365d97806281cfbc3eb73265d
mandajm <etudiant42@sayna.io>
2019-01-04 23:54:00 +0100

    mon super pyramide

---
+++ ruby/index.rb
@@ -0,0 +1,10 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+k = gets.chomp.to_i
+puts "Voici le pyramide"
+y = k
+star="#"
+puts star
+	k.times do
+	star+="#"
+ 	puts star
+end
